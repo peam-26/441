43d72df574fe682ec2373999653183255c2928bf
peam-26 <peampat.suphaphon@gmail.com>
2025-10-13 16:21:53 -0400

    Update shifter.py

--- shifter.py
+++ shifter.py
@@ -1,20 +1,38 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BCM)
-dataPin, latchPin, clockPin = 23, 24, 25
-GPIO.setup(dataPin, GPIO.OUT)
-GPIO.setup(latchPin, GPIO.OUT, initial=0) # start latch & clock low
-GPIO.setup(clockPin, GPIO.OUT, initial=0)
-pattern = 0b01111111 # pattern to display
-for i in range(8):
-	GPIO.output(dataPin, pattern & (1<<i))
-	GPIO.output(clockPin,1) # ping the clock pin to shift register data
-	time.sleep(0)
-	GPIO.output(clockPin,0)
-GPIO.output(latchPin, 1) # ping the latch pin to send register to output
-time.sleep(0)
-GPIO.output(latchPin, 0)
+
+class Shifter:
+	def __init__(self, serial, latch, clock):
+		self.dataPin = serial
+		self.latchPin = latch
+		self.clockPin = clock
+
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(self.dataPin, GPIO.OUT)
+		GPIO.setup(self.latchPin, GPIO.OUT, initial=0) # start latch & clock low
+		GPIO.setup(self.clockPin, GPIO.OUT, initial=0)
+
+	def ping(self, p): #based on public ping() from lecture:
+		GPIO.output(p,1)
+		time.sleep(0)
+		GPIO.output(p,0)
+
+	def shiftByte(self, b): # send a byte of data to the output
+    	for i in range(8):
+       		GPIO.output(self.dataPin, b & (1 << i))
+        	self.ping(self.clockPin)  # add bit to register
+    	self.ping(self.latchPin)  # send register to output
+
+
+
+
 try:
-	while 1: pass
+	test = Shifter(23,24,25)
+    while 1:
+        for i in range(2**8):
+            test.shiftByte(i)
+            time.sleep(0.5)
 except:
-	GPIO.cleanup()
+    GPIO.cleanup()
+
+
