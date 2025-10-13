e3e092ad71c6c899120f9b52009f608a6d2a0db0
peam-26 <peampat.suphaphon@gmail.com>
2025-10-13 16:48:23 -0400

    Create bug.py

---
+++ bug.py
@@ -0,0 +1,29 @@
+from shifter import Shifter
+
+import RPi.GPIO as GPIO
+import time
+import random
+
+
+s = Shifter(serial=23, latch=24, clock=25)
+
+pos = 0  # starting led position (1st led)
+
+try:
+    while True:
+        s.shiftByte(1 << pos)
+        time.sleep(.05)
+
+        step = random.choice((-1, 1))
+        nxtpos = pos + step
+
+        # Bounce if you hit the edges (keep inside 0–7)
+        if nxtpos < 0 or nxtpos >= LED_COUNT:
+            step = -step
+            nxtpos = pos + step
+
+        pos = nxtpos
+
+except:
+    s.shiftByte(0)  
+    GPIO.cleanup()
